f16569c2a888acf599745a29c8a41803da592788
Daniel Sank <sank.daniel@gmail.com>
2017-09-29 17:51:15 -0700

    Use Self type!

--- monad/src/main.rs
+++ monad/src/main.rs
@@ -1,22 +1,21 @@
-trait Comparable<T: Comparable<T>> {
-    fn compare(&self, other: T) -> bool;
-}
-
-// C implements comparable
-struct C {}
-
-impl Comparable<C> for C {
-    fn compare(&self, other: C) -> bool {true}
-}
-
 /*
 trait Monad<M,A> {
     fn flatmap<B,C: Monad<M,B>>(&self, f: fn(A) -> C) -> C;
 }
 */
 
+trait Comparable {
+    fn compare(self: &Self, other: &Self) -> bool;
+}
+
+struct C {}
+
+impl Comparable for C {
+    fn compare(&self, other: &C) -> bool {true}
+}
+
 fn main() {
     let c = C {};
     let cc = C {};
-    println!("Comparison: {:?}", c.compare(cc));
+    println!("Comparison: {:?}", c.compare(&cc));
 }
